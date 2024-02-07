4959002cb7dd4d5c595509df45d8034e17adaf8c
Tatusaily <tatu.sailynoja@gmail.com>
2024-02-07 13:40:43 +0200

    pohja

---
+++ Simulator/src/Distributions/ContinuousGenerator.java
@@ -0,0 +1,7 @@
+package Distributions;
+
+/** A continuous generator provides a double value according to the distribution it relies on.
+ */
+public interface ContinuousGenerator extends Seedable {
+    double sample(); 
+}
---
+++ Simulator/src/Distributions/DiscreteGenerator.java
@@ -0,0 +1,7 @@
+package Distributions;
+
+/** A continuous generator provides a long value according to the distribution it relies on.
+ */
+public interface DiscreteGenerator extends Seedable {
+    long sample(); 
+}
